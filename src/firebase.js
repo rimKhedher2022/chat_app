deafd9895ebe94652fa0593ba5fd757ad679c710
rimKhedher2022 <rimkhedher0@gmail.com>
2022-11-02 23:41:29 +0100

    commit1

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import {getAuth} from 'firebase/auth';
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyBMu-OvVdL_wn6aDUCiLpwE39buwKTzg6E",
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Add from "../img/addimage.png"
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import {auth} from "../firebase"
 
 const Register = () => {
 
@@ -8,7 +10,21 @@ const Register = () => {
     const displayName = e.target[0].value;
     const email = e.target[1].value;
     const password = e.target[2].value;
-    const file = e.target[3].value;
+    const file = e.target[3].files[0];
+
+
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
   }
 
 
@@ -20,7 +36,7 @@ const Register = () => {
           <span className='logo'>Chat app</span>
           <span className='title'>Register</span>
           <form onSubmit={handleSubmit}>
-            <input type="text" placeholder='display name'/>
+            <input type="text" placeholder='display name' />
             <input type="email" placeholder='email'/>
             <input type="password" placeholder='password'/>
             <input style={{display:"none"}} type='file' id='file'/>
