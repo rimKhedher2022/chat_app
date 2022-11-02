41c222e389c880b2b97b551715dc3e2b53602d95
rimKhedher2022 <rimkhedher0@gmail.com>
2022-11-02 19:21:11 +0100

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div className="navbar">
       <span className="logo"> Chat </span>
       <div className="user">
-        <img src=''  alt=''/>
+        <img src="https://i11.haber7.net//haber/haber7/photos/2020/53/Ues00_1609399241_7764.jpg"  alt=''/>
         <span>jhon</span>
         <button>logout</button>
       </div>
