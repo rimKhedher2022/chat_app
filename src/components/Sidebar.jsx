7a253bb9af0611cd871a05f37b4776d89f8e7f6a
rimKhedher2022 <rimkhedher0@gmail.com>
2022-11-02 19:28:16 +0100

    test

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Search from './Search'
+import Chats from './Chats'
 
 const Sidebar = () => { 
   return (
     <div>
       <div className='sidebar'>
       <Navbar/> 
+      <Search/>
+      <Chats/>
 
       </div>
      
