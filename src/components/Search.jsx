8e1be7001682b8693bc1ea31030be3b9a5937eeb
rimKhedher2022 <rimkhedher0@gmail.com>
2022-11-02 20:21:16 +0100

    commit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,7 +6,7 @@ const Search = () => {
         <div className="searchForm">
             <input type="text" />
         </div>
-
+        {/* maintenat on ajoute une commit */}
         <div className='userChat'>
             <img src='https://i11.haber7.net//haber/haber7/photos/2020/53/Ues00_1609399241_7764.jpg' />
             <div className='userChatInfo'>
