ed05b9f059e30d56b12d1f441eef938d661954ca
rimKhedher2022 <rimkhedher0@gmail.com>
2022-11-02 21:39:33 +0100

    commit

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import Img from "../img/img.png";
+import Attach from "../img/attach.png";
 
 const Input = () => {
   return (
     <div className='input'>
-        <input type="text" placeholder='' />
-      
+        <input type="text" placeholder='type a message ... '/>
+        <div className="send">
+            <img src={Attach} alt=''/>
+            <input type="file" style={{display:"none"}} id="file" />
+            <label htmlFor='file'>
+                <img src={Img} alt=''/>
+            </label>
+            <button>send</button>
+        </div>
     </div>
   )
 }
---
+++ src/components/Message.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Message = () => {
+  return (
+    <div>
+     <div className="message">
+        <div className="messageInfo">
+            <img src=''/>
+        </div>
+
+
+     </div>
+    </div>
+  )
+}
+
+export default Message
