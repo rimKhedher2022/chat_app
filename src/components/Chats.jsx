63ffec848c9688e4c49b229c0ad3f508c325c4ef
rimKhedher2022 <rimkhedher0@gmail.com>
2022-11-02 20:46:52 +0100

    commit

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -2,9 +2,35 @@ import React from 'react'
 
 const Chats = () => {
   return (
-    <div>
-      chats
+    <div className='chats'>
+       <div className='userChat'>
+            <img src='https://i11.haber7.net//haber/haber7/photos/2020/53/Ues00_1609399241_7764.jpg' alt=''/>
+            <div className='userChatInfo'>
+                    <span>jane</span>
+                    <p>Hello</p>
+            </div>
+        </div>
+       <div className='userChat'>
+            <img src='https://i11.haber7.net//haber/haber7/photos/2020/53/Ues00_1609399241_7764.jpg' alt=''/>
+            <div className='userChatInfo'>
+                    <span>jane</span>
+                    <p>Hello</p>
+            </div>
+        </div>
+       <div className='userChat'>
+            <img src='https://i11.haber7.net//haber/haber7/photos/2020/53/Ues00_1609399241_7764.jpg' alt=''/>
+            <div className='userChatInfo'>
+                    <span>jane</span>
+                    <p>Hello</p>
+            </div>
+        </div>
     </div>
+
+
+      
+
+
+
   )
 }
 
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,13 +4,13 @@ const Search = () => {
   return (
     <div className='search'>
         <div className="searchForm">
-            <input type="text" />
+            <input type="text"  placeholder='find a user'/>
         </div>
-        {/* maintenat on ajoute une commit */}
+       
         <div className='userChat'>
             <img src='https://i11.haber7.net//haber/haber7/photos/2020/53/Ues00_1609399241_7764.jpg' />
             <div className='userChatInfo'>
-
+                <span>jane</span>
             </div>
 
 
