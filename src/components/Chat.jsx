d33ef3eb95ce3bf86c5215380619cd265e816b36
rimKhedher2022 <rimkhedher0@gmail.com>
2022-11-02 21:23:31 +0100

    commit

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,18 +2,22 @@ import React from 'react'
 import Cam from "../img/cam.png"
 import Add from "../img/addl.png"
 import More from "../img/more.png"
+import Messages from './Messages'
+import Input from './Input'
 
 const Chat = () => {
   return (
     <div className='chat'>
-      <div className='chatInfo'>
-        <span>Jane</span>
-          <div className='chatIcons'>
-            <img src={Cam} alt=""/>
-            <img src={Add} alt=""/>
-            <img src={More} alt=""/>
+          <div className='chatInfo'>
+            <span>Jane</span>
+              <div className='chatIcons'>
+                <img src={Cam} alt=""/>
+                <img src={Add} alt=""/>
+                <img src={More} alt=""/>
+              </div>
           </div>
-      </div>
+      <Messages/>
+      <Input/>
     </div>
   )
 }
